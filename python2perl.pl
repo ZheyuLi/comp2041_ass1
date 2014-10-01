7f28539a77675dce0cfb67bc9fd1a41f96c19e85
Charbel Antouny <charbel.antouny@gmail.com>
2014-10-02 03:16:45 +1000

    Completed subset 1

--- python2perl.pl
+++ python2perl.pl
@@ -7,34 +7,48 @@ if (!@ARGV) {
     push @ARGV, "-";
 }
 
-sub perlVar {
-    my $var = $_[0];
-    return "\$$var";
-}
+# sub perlVar {
+#     my $var = $_[0];
+#     return "\$$var";
+# }
 
-my %vars = ();
+$regexVar = "[a-zA-Z][a-zA-Z0-9_]*";
+#my %vars = ();
 foreach my $file (@ARGV) {
     open F, "<$file";
     while ($line = <F>) {
         if ($line =~ m/^#!/ && $. == 1) {
             # translate #! line 
             print "#!/usr/bin/perl -w\n";
+
         } elsif ($line =~ m/^\s*#/ || $line =~ /^\s*$/) {
             # Blank & comment lines can be passed unchanged
             print $line;
+
         } elsif ($line =~ m/^\s*print\s*"(.*)"\s*$/) {
             # Python's print print a new-line character by default
             # so we need to add it explicitly to the Perl print statement
             print "print \"$1\\n\";\n";
-        } elsif ($line =~ m/^\s*print\s*([a-zA-Z][a-zA-Z0-9_]*)\s*$/) {
+
+        } elsif ($line =~ m/^\s*print\s*($regexVar)\s*$/) {
             # print var instead of string
             print "print \"\$$1\\n\";", "\n";
-            #check if var exists in hash then print
-        } elsif ($line =~ m/^\s*([a-zA-Z][a-zA-Z0-9_]*)\s*=\s*([0-9]*)/) {
-            # captures numeric variables
-            my $var = perlVar($1);
-            print "$var = $2;\n";
-            $vars{"$var"} = "$2";
+            
+        } elsif ($line =~ m/^\s*($regexVar)\s*(\+|-)?=\s*([0-9]+|$regexVar)/) {
+            # captures numeric and variable assignment
+            # also checks for += or -=
+            my $var = "\$$1";
+            my $operator = ($2) ? ("$2") : ("");
+            my $value = "$3";
+            if ($line =~ m/(([0-9]*|$regexVar)\s*(\+|-|\*|\/|%|\*\*)\s*([0-9]+|$regexVar))+/) {
+                # captures arithmetic equations
+                $value = "$&";
+                $value =~ s/($regexVar)/\$$1/g;
+                #$vars{"$var"} = eval "$value";
+            }
+            print "$var ${operator}= ${value};", "\n";
+            #$vars{"$var"} = "$value" if (!defined $vars{"var"});
+
         } else {
             # Lines we can't translate are turned into comments
             print "#$line\n";
---
+++ test/test00.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+variable = 1 + 7 * 7 - 8 % 3 + 2 ** 3 - 8 / 2 - 12
+variable += 2
+print variable
